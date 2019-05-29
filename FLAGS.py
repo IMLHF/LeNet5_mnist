7f98a4ba6bfd0864f390c3520fb60abc60cb46a6
IMLHF <red_wind@foxmail.com>
2019-05-29 17:34:42 +0800

    # add

--- FLAGS.py
+++ FLAGS.py
@@ -37,10 +37,26 @@ class C002(base_config):
 
 
 class C003(base_config):
-  CHECK_POINT = 'nnet_C003'
-  ACTIVATION = tf.nn.sigmoid
+  CHECK_POINT = 'nnet_LeNet5_C003'
+  CONV1_FILTERS = 6
+  CONV2_FILTERS = 16
+  FC1_units = 120
+  FC2_units = 84
+  FC3_units = 10
+  DROP_RATE = 0.1
+  ACTIVATION = tf.nn.tanh
   OPTIMIZER = tf.train.AdamOptimizer
 
 
+class C004(base_config):
+  CHECK_POINT = 'nnet_LeNet5_C004'
+  CONV1_FILTERS = 6
+  CONV2_FILTERS = 16
+  FC1_units = 120
+  FC2_units = 84
+  FC3_units = 10
+  DROP_RATE = 0.5
+  ACTIVATION = tf.nn.tanh
+  OPTIMIZER = tf.train.AdamOptimizer
 
-PARAM = C002
+PARAM = C001
